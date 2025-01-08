e853f94d750b72cbabc5a1580c7a408457deea13
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2025-01-08 17:20:30 -0500

    otra prueba de consola de c#

---
+++ PruebaVB/ConsolePrueba/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+var regla = Console.ReadLine();
+var texto = Console.ReadLine();
+if (texto == null || regla == null)
+{
+
+    regla = @"(a+)+";
+    texto = "a";
+}
+
+var respuesta = Regex.IsMatch(texto, regla);
+Console.WriteLine(respuesta);
+
